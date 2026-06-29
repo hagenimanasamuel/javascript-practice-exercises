3c748abf7c3617e693fcd6c2f7493ce0f51b473e
hagenimanasamuel <smlhagenimana@gmail.com>
2026-06-29 19:22:53 +0200

    updated the top three file to also handles the characters not alphabets as space

--- 03-katas/topThreeWords.js
+++ 03-katas/topThreeWords.js
@@ -1,13 +1,15 @@
 function topThreeWords(text) {
-    let textArray = text.split(" ");
+    let cleanText = text.toLowerCase().replace(/[^a-z']/g, ' ');
+    let textArray = cleanText.split(' ');
     let wordCount = {};
     
     for (let i = 0; i < textArray.length; i++) {
-        if (textArray[i].length > 0) {
-            if (wordCount[textArray[i]]) {
-                wordCount[textArray[i]]++;
+        let word = textArray[i];
+        if (word.length > 0 && word.replace(/'/g, '').length > 0) {
+            if (wordCount[word]) {
+                wordCount[word]++;
             } else {
-                wordCount[textArray[i]] = 1;
+                wordCount[word] = 1;
             }
         }
     }
@@ -17,8 +19,7 @@ function topThreeWords(text) {
     });
     
     let FirstThree = sorted.slice(0, 3);
-    
     return FirstThree;
 }
 
-console.log(topThreeWords("In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income."))
+console.log(topThreeWords("In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income."));
